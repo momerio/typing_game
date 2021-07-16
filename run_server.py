308552a0d44d831ee285245dfd1cdc6f723c3612
momerio <moment.namanishi@gmail.com>
2021-07-16 17:25:46 +0900

    changed ip

--- run_server.py
+++ run_server.py
@@ -1,13 +1,13 @@
 """
 run test server
 
-http://localhost:8000/
+http://localhost:8888/
 """
 
 import http.server
 
 print("run server")
-server_address = ("", 8000)
+server_address = ("", 8888)
 handler_class = http.server.CGIHTTPRequestHandler
 server = http.server.HTTPServer(server_address, handler_class)
 server.serve_forever()
